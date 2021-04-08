63dab17eacfafbca5d4a17427660b3a8671532ef
Oudani Achraf <achraf.oudani@gmail.com>
2021-04-08 07:31:36 +0200

    update shelf

--- src/Books.js
+++ src/Books.js
@@ -1,58 +1,57 @@
 import React, { Component } from "react";
 
 class Books extends Component {
-  state = {
-    currentShelf: "",
-  };
   render() {
     return (
       <ol className="books-grid">
-        {this.props.BooksList.filter(
-          (book) => book.shelf === this.props.shelf
-        ).map((book) => {
-          console.table(book);
-          return (
-            <li key={book.id}>
-              <div className="book">
-                <div className="book-top">
-                  <div
-                    className="book-cover"
-                    style={{
-                      width: 128,
-                      height: 193,
-                      backgroundImage: `url(${book.imageLinks.thumbnail})`,
-                    }}
-                  />
-                  <div className="book-shelf-changer">
-                    <select
-                      onChange={(e) => {
-                        this.props.updateshelf(book, e.target.value);
-                        console.log(this.state.currentShelf);
+        {this.props.BooksList.length > 0 &&
+          this.props.shelf &&
+          this.props.BooksList.filter(
+            (book) => book.shelf === this.props.shelf
+          ).map((book) => {
+            console.table(book);
+            return (
+              <li key={book.id}>
+                <div className="book">
+                  <div className="book-top">
+                    <div
+                      className="book-cover"
+                      style={{
+                        width: 128,
+                        height: 193,
+                        backgroundImage: `url(${book.imageLinks.thumbnail})`,
                       }}
-                      value={this.state.currentShelf}
-                    >
-                      <option value="move" disabled>
-                        Move to...
-                      </option>
-                      <option value="currentlyReading">
-                        Currently Reading
-                      </option>
-                      <option value="wantToRead">Want to Read</option>
-                      <option value="read">Read</option>
-                      <option value="none">None</option>
-                    </select>
+                    />
+                    <div className="book-shelf-changer">
+                      <select
+                        onChange={(e) => {
+                          this.props.updateshelf(book, e.target.value);
+                          console.log("target", e.target.value);
+                        }}
+                        value={this.props.shelf}
+                      >
+                        <option value="move" disabled>
+                          Move to...
+                        </option>
+                        <option value="currentlyReading">
+                          Currently Reading
+                        </option>
+                        <option value="wantToRead">Want to Read</option>
+                        <option value="read">Read</option>
+                        <option value="none">None</option>
+                      </select>
+                    </div>
                   </div>
+                  <div className="book-title">{book.title}</div>
+                  {book.authors.map((author) => (
+                    <div className="book-authors" key={book.id}>
+                      {author}
+                    </div>
+                  ))}
                 </div>
-                <div className="book-title">{book.title}</div>
-                {book.authors.map((author) => (
-                  <div className="book-authors" key={book.id}>
-                    {author}
-                  </div>
-                ))}
-              </div>
-            </li>
-          );
-        })}
+              </li>
+            );
+          })}
       </ol>
     );
   }
---
+++ src/Shelf.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+class Shefl extends Component {
+  render() {
+    return (
+      <div className="list-books-content">
+        <div>
+          <div className="bookshelf">
+            <h2 className="bookshelf-title">{this.props.title}</h2>
+            <div className="bookshelf-books">{this.props.Books}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Shefl;
